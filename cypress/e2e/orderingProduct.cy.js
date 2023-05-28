109bce1b97413e7e61c52467b7d3f91ca71349e6
Vladislav Nagornyi <nagornyi.vo@gmail.com>
2023-05-28 16:21:00 +0300

    update selectors

--- cypress/e2e/orderingProduct.cy.js
+++ cypress/e2e/orderingProduct.cy.js
@@ -4,15 +4,15 @@ describe('orderingProduct', () => {
     // open website
     cy.visit("https://www.demoblaze.com/");
     // go to product page
-    cy.contains('Samsung galaxy s6').click();
+    cy.contains('a.hrefch', 'Samsung galaxy s6').click();
     // click on add to cart
     cy.location("href").should("eq", "https://www.demoblaze.com/prod.html?idp_=1");
     // The product has been added to the cart and click "ok"
-    cy.contains('Add to cart').click();
-      // click cart button
-    cy.contains('Cart').click();
+    cy.contains('a[onclick="addToCart(1)"]', 'Add to cart').click();
+    // click cart button
+    cy.contains('a.nav-link#cartur', 'Cart').click();
     // click place order button
-    cy.contains('Place Order').click();
+    cy.contains('button[data-target="#orderModal"]', 'Place Order').click();
     // filling in the order fields 
     cy.get('#name').click().type('name');
     cy.get('#country').click().type('USA');
@@ -21,7 +21,7 @@ describe('orderingProduct', () => {
     cy.get('#month').click().type('01');
     cy.get('#year').click().type('2001');
     // // click Purchase button
-    cy.contains('Purchase').click();
-    cy.contains('OK').click();
+    cy.contains('button[onclick="purchaseOrder()"]', 'Purchase').click();
+    cy.contains('button.confirm', 'OK').click();
   });
 });
--- cypress/e2e/productToCard.cy.js
+++ cypress/e2e/productToCard.cy.js
@@ -4,10 +4,10 @@ describe('productToCard', () => {
     // open website
     cy.visit("https://www.demoblaze.com/");
     // go to product page
-    cy.contains('Samsung galaxy s6').click();
+    cy.contains('a.hrefch', 'Samsung galaxy s6').click();
     // click on add to cart
     cy.location("href").should("eq", "https://www.demoblaze.com/prod.html?idp_=1");
     // The product has been added to the cart and click "ok"
-    cy.contains('Add to cart').click();
+    cy.contains('a[onclick="addToCart(1)"]', 'Add to cart').click();
   })
 })
